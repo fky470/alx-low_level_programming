409f8a599fccbed8e3d0cc8f6aa1b59d3bdfea4a
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-10 13:26:21 -0800

    adding 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
+}
