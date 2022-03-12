dcf5e7d077c7d8ce085b8e4986666dd68979f480
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-11 22:41:54 -0800

    modifying 2-print_alphabet.c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,14 @@
 
 int main(void)
 {
-    for ( char c = 'a'; c <= 'z'; c++ )
+    int c;
+    c = 'a';
+    while(c <= 'z')
     {
         putchar(c);
+        c++;
     }
     putchar('\n');
+
+    return(0);
 }
