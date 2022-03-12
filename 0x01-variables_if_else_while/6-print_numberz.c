16d30205d6b0a9f8a68f05d642d92b22b1722e99
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-12 01:21:40 -0800

    adding 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 49; d <= 57; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
