825e0b30afc7d7a8f0ac376e3ead154ba56e4d03
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-14 10:12:54 -0700

    modifying 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,10 +25,12 @@ int main(void)
 						putchar(' ');
 						putchar(z);
 						putchar(d);
+						
 						if (x + y + z + d == 227 && x == 57)
 						{
 							break;
 						}
+						
 						else
 						{
 							putchar(',');
