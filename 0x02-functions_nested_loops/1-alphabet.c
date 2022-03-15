3474555a31ed331cd0d8d2108960071844850e73
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-15 09:17:00 -0700

    adding 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students
+ * print_alphabet - check the code for Holberton School students
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	int d;
 
