48320fa6d5b4eb69de081f314e49c7243f12d0e6
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-15 09:09:08 -0700

    adding 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 97; d <= 122; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+	return (0);
+}
