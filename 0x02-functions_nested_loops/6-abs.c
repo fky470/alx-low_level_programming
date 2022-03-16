34bcd09200db483bb6ea48d78e7d023aee4dc31e
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-16 03:16:00 -0700

    adding 6-abs.c file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 
 /**
  *  _abs - function that prints the absolute value of a number
- *  @n - takes in an integer
- *  return - + if n < 0, - if n > 0, 0 if n = 0
+ *  @n: takes in an integer
+ *  Return: 1 if > 0, 0 if = 0, positive if negative
  */
 
 int _abs(int n)
