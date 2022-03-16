12d71789d3a6f3f9822824fddf32bd9e46df5b07
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-16 08:06:46 -0700

    adding 10-add.c file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,8 +4,9 @@
  * add - function that adds two integers together
  * @x: takes in an integer
  * @y: takes in an integer
- * return: x + y result
+ * Return: x + y result
  */
+
 int add(int x, int y)
 {
 	return (x + y);
