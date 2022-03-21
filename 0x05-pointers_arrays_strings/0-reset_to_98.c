108c73072a3e072a859992bb95d585bf670ccce8
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-21 08:08:44 -0700

    adding 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to 98
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
  * @n: parameter to take value
  */
 
