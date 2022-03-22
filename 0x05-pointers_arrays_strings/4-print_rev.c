f87b52ca75b98d70d9d4da206998af2f98fdd479
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-22 04:35:55 -0700

    adding 3-puts.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_rev - print a string,in reverse
+ * @s: the string
+ */
+
 void print_rev(char *s)
 {
 	int i = 0;
@@ -12,4 +17,7 @@ void print_rev(char *s)
 	while (i > 0)
 	{
 		i--;
+		_putchar(s[i]);
 	}
+	_putchar('\n');
+}
