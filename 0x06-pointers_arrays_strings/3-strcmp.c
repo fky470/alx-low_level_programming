cfe01bd60ebd3c41296f5eab7ebe5f944a7d9d54
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-24 10:16:21 -0700

    modifying 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i, R;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
+		{
 			break;
-		else if (s1[i] == '\0')
+		}
 
 	}
+	R = s1[i] - s2[i];
+
+	return (R);
 }
