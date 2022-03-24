d085ffe7092df005065ba232a120aebe07e12eaf
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-24 10:19:40 -0700

    modifying 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,17 +2,17 @@
 
 /**
  * _strcmp - function that compares two strings
+ *
  * @s1: the string
  * @s2: the string
- * Return: return  0 if s1 and s2 are equal a negative value if s1 is less than s2
- * a positive value if s1 is greater than s2
+ * Return: return  0
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i, R;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] == s2[i]; i++)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 		{
