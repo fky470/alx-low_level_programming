f0689c388814212dcf31c3b45e8fe93b142e740e
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-25 02:32:27 -0700

    adding 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,51 +12,25 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i >= 'a' && i <= 'z')
+		if (s[i] == 'a' || s[i] == 'A')
 		{
-			if (s[i] == 'a')
-			{
-				s[i] = '4';
-			}
-			else if (s[i] == 'e')
-			{
-				s[i] = '3';
-			}
-			else if (s[i] == 'o')
-			{
-				s[i] = '0';
-			}
-			else if (s[i] == 't')
-			{
-				s[i] = '7';
-			}
-			else if (s[i] == 'l')
-			{
-				s[i] = '1';
-			}
+			s[i] = '4';
 		}
-		else
+		else if (s[i] == 'e' || s[i] == 'E')
 		{
-			if (s[i] == 'A')
-			{
-				s[i] = '4';
-			}
-			else if (s[i] == 'E')
-			{
-				s[i] = '3';
-			}
-			else if (s[i] == 'O')
-			{
-				s[i] = '0';
-			}
-			else if (s[i] == 'T')
-			{
-				s[i] = '7';
-			}
-			else if (s[i] == 'L')
-			{
-				s[i] = '1';
-			}
+			s[i] = '3';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
 		}
 	}
 	return (s);
