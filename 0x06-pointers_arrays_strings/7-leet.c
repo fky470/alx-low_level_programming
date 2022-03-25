1425013295034bc39b82f315e9a43549b3975b94
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-25 05:28:02 -0700

    adding 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * leet -  function that encodes a string into 1337
- * @s: the string
+ * leet - function that encodes a string into 1337
+ * @s: input
  * Return: return string
  */
 
