cdfb01005d69a9e56db29902db5b93d3601828b8
fky470 <fikreyohannesbiruk@gmail.com>
2022-03-28 06:29:54 -0700

    adding 0-memset.c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: the string
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: return s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		return (s);
+	}
+}
