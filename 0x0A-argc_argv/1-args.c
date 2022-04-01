f5dc624ceb5873aed63960d3576dadd6aaa77d48
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-01 07:34:42 -0700

    adding 1-args.c file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,9 @@
  * Return: return 0
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
-	{
 		printf("%d\n", argc - 1);
-
-		return (0);
-	}
+	return (0);
 }
