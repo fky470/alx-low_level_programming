67400d259a117b83bce498079ad9e7c5a6d46692
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-01 04:43:13 -0700

    adding 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv)
+/**
+ * main - main function
+ * argc: count argument
+ * *argv: size of argc
+ * Return: success(0)
+ */
+
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
