d9608daff85a5e019619c1886ad9031ea242bc9c
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-02 03:30:08 -0700

    adding 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,29 +14,21 @@ int main(int argc, char *argv[])
 	int i, d;
 	int sum = 0;
 
-	if (argc <= 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
+		for (d = 0 ; argv[i][d]; d++)
 		{
-			for (d = 0 ; argv[i][d]; d++)
+			if (argv[i][d] < '0' || argv[i][d] > '9')
 			{
-				if (argv[i][d] < '0' || argv[i][d] > '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-
-			sum = sum + atoi(argv[i]);
 		}
 
-		printf("%d\n", sum);
-
-		return (0);
+		sum = sum + atoi(argv[i]);
 	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
