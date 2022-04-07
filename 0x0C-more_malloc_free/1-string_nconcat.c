148a9af3ef6f708181132163e27599049487f07f
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-07 08:29:25 -0700

    adding 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: an unsigned integer
- * Return: a pointer or null
+ * Return: char pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
