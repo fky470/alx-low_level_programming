f2032ad7f4c667ee399ab07e15d39f9211e2f77c
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-07 08:33:26 -0700

    adding 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,31 +20,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
+
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
+
 	if (n >= len2)
 	{
 		n = len2;
 	}
 	p = malloc((len1 + n + 1) * sizeof(char));
+
 	if (p == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
+
 	for (i = 0; i < len1; i++)
 	{
 		p[i] = s1[i];
 	}
+
 	for (j = 0; j < len1 + n; j++, i++)
 	{
 		p[j] = s2[j];
 	}
 	p[j] = '\0';
+
 	return (p);
 }
