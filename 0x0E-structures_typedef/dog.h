fca7e64374236b688889fbcd944067d62aab8ecb
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-11 09:37:40 -0700

    adding dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
-#ifndef STRUCT_DOG
-#define STRUCT_DOG
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 
 struct dog
 {
-	char *name;
+	char* name;
 	float age;
-	char *owner;
+	char* owner;
 };
 
 #endif
