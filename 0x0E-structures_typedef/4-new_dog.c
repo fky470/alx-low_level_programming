510cf798b43b0bd98c4ce20e3a9e741178461143
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-12 08:19:29 -0700

    modifing file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,8 +24,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
 		free(d);
 		return (NULL);
+	}
 
 	d->name = malloc(sizeof(d->name) * i);
 	if (d->name == NULL)
