6f6ad5fb7c401469da2e7702d0edc4010ac36fd3
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-12 02:07:32 -0700

    adding 5-free_dog.c file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - Frees dog
+ * @d: dog to be freed
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
