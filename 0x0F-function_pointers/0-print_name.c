33abb37cf373768ea271b34b60142ea8b8a8aaca
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-13 08:49:04 -0700

    adding 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: a string
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+my alx 0x0F.c -function_pointers project
