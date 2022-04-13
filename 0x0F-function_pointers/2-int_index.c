be9c14c0c943f34e78fa1788701c1d1de8a7e806
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-13 10:36:15 -0700

    adding 2-int_index.c file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1 || cmp != NULL || array != NULL)
+	if (size < 1 || cmp == NULL || array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
