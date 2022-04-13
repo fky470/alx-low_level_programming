dd82f0bd91961df05c9b26e06b5d3a7604a4ec20
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-13 10:26:06 -0700

    adding 2-int_index.c file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: an array of integers
+ * @size: size of an array
+ * @cmp: function pointer
+ * Return: index of first element that
+ * matches with `cmp`, or -1 if none found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || cmp != NULL || array != NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		return (i);
+	}
+	return (-1);
+}
