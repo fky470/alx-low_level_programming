6d4a27f178c4de52e2c2362d1568a70a4261d182
fky470 <fikreyohannesbiruk@gmail.com>
2022-04-20 05:35:56 -0700

    adding 9-strcpy.c file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strcpy - copies the string
+ * @dest: a string
+ * @src: a string
+ * Return: return a pointer
+ */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int x;
+	int i;
 
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
 
+	return (dest);
+}
